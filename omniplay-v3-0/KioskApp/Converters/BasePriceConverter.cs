48057674dfe7c030b04dbf5828bc00353d6b164f
nseckinoral <nseckinoral@gmail.com>
2015-01-30 19:37:05 +0200

    Wishlist Implementation
    NOTE: This implementation is WIP
    
    -Added Ratings for wishlist items
    -Added Prices for wishlist items (WIP)
    -Wishlist item images are now smaller.

---
+++ omniplay-v3-0/KioskApp/Converters/BasePriceConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace App1.Converters
+{
+    public class BasePriceConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (Math.Truncate((double)value));
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ omniplay-v3-0/KioskApp/Converters/DecimalPriceConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace App1.Converters
+{
+    public class DecimalPriceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var doubleValue = (double)value; 
+            //var decimalValue = doubleValue-Math.Truncate(doubleValue);
+            //var result = Math.Truncate(decimalValue*100);
+            return doubleValue.ToString("0:0.#");
+            //return result ;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
