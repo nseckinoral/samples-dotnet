4fc2e8842e1f5abc764c4f4bb40d0f1916039008
Daron Yondem <daron@yondem.com>
2015-01-30 23:37:28 +0200

    Formatted and removed misinforming comments.

--- omniplay-v3-0/KioskApp/Converters/ProgressValueToRatingConverter.cs
+++ omniplay-v3-0/KioskApp/Converters/ProgressValueToRatingConverter.cs
@@ -9,21 +9,14 @@ namespace App1.Converters
 {
     public class ProgressValueToRatingConverter : IValueConverter
     {
-        // This converts the DateTime object to the string to display.
-        public object Convert(object value, Type targetType,
-            object parameter, string language)
+        public object Convert(object value, Type targetType, object parameter, string language)
         {
-
             return (double)value * 20;
-
         }
 
-        // No need to implement converting back on a one-way binding 
-        public object ConvertBack(object value, Type targetType,
-            object parameter, string language)
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
         }
     }
-
 }
