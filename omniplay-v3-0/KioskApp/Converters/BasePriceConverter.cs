e6800a6f0d2b6c96036c6661e5252473d1b232a7
Daron Yondem <daron@yondem.com>
2015-01-30 23:35:43 +0200

    Removed spaces.

--- omniplay-v3-0/KioskApp/Converters/BasePriceConverter.cs
+++ omniplay-v3-0/KioskApp/Converters/BasePriceConverter.cs
@@ -9,17 +9,14 @@ namespace App1.Converters
 {
     public class BasePriceConverter : IValueConverter
     {
-
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             return (Math.Truncate((double)value));
-            
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
         {
             throw new NotImplementedException();
         }
-
     }
 }
