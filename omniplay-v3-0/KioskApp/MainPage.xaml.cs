2d4d03568c7578821de2ea19b4c3f5dc5c5ce73e
Daron Yondem <daron@yondem.com>
2015-01-31 00:10:55 +0200

    Removed unused usings.

--- omniplay-v3-0/KioskApp/MainPage.xaml.cs
+++ omniplay-v3-0/KioskApp/MainPage.xaml.cs
@@ -1,29 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using Windows.Storage;
 using Windows.Storage.Streams;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using XOMNI.SDK.Public;
 using XOMNI.SDK.Public.Clients.Utility;
 using Windows.UI.Xaml.Media.Imaging;
 using System.Threading.Tasks;
-using XOMNI.SDK.Public.Clients.Company;
-using XOMNI.SDK.Public.Clients.OmniPlay;
 using XOMNI.SDK.Public.Clients.PII;
 using XOMNI.SDK.Public.Models.OmniPlay;
-using Windows.UI.Popups;
-using XOMNI.SDK.Public.Models.PII;
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace App1
